671b0cfe695c3cb7a3df39331eb9d202a38c1236
Kristian Fulkerson <kristianf451@gmail.com>
2022-03-22 12:21:13 -0700

    header done, need to finish mobile design, and hamburger menu

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import {
   SearchIcon,
@@ -7,11 +8,15 @@ import {
   PaperAirplaneIcon,
   MenuIcon,
 } from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
+import mypic from '../assets/day_ryder.jpeg'
 
 function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+
   return (
-    <div>
-      <div className="flex max-w-6xl justify-between bg-white">
+    <div className="sticky top-0 z-50 border-b bg-white shadow-sm">
+      <div className="mx-5 flex max-w-6xl justify-between bg-white lg:mx-auto">
         {/* Left */}
         <div className="relative hidden w-24 cursor-pointer lg:inline-grid">
           <Image
@@ -42,7 +47,46 @@ function Header() {
           </div>
         </div>
 
-        {/* right */}
+        {/* Right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <MenuIcon
+            onClick={() => setIsNavOpen((prev) => !prev)}
+            className="h-6 cursor-pointer md:hidden"
+          />
+          <div className="navBtn relative">
+            <PaperAirplaneIcon className="navBtn" />
+            <div className="absolute -top-2 -right-0 flex h-4 w-4 animate-pulse items-center justify-center rounded-full bg-red-500 text-xs text-white">
+              3
+            </div>
+          </div>
+
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+          <div className="navBtn h-10 cursor-pointer">
+            <Image
+              src={mypic}
+              alt="profile pic"
+              height={40}
+              width={40}
+              className="rounded-full"
+              objectFit="contain"
+            />
+          </div>
+          {/* TODO: Get hamburger menu working */}
+          {isNavOpen && (
+            <div className="flex items-center">
+              <HomeIcon className="h-10 w-10" />
+              <div className="relative">
+                <PaperAirplaneIcon className="h-10 w-10" />
+                <div className="absolute -top-2 -right-0 flex h-4 w-4 animate-pulse items-center justify-center rounded-full bg-red-500 text-xs text-white">
+                  3
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
