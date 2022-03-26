3e3f856ba33db73c78e085520029f5bc3b17a4f7
Kristian Fulkerson <kristianf451@gmail.com>
2022-03-25 23:55:38 -0700

    now pulling post data from firebase firestore instead of hard coded dummy data, all working

--- components/Posts.js
+++ components/Posts.js
@@ -1,41 +1,32 @@
 import Post from './Post'
-import { faker } from '@faker-js/faker'
-
-const posts = [
-  {
-    id: '123',
-    username: 'krisf451',
-    userImg: faker.image.avatar(),
-    img: faker.image.avatar(),
-    caption: 'YOOOO',
-  },
-  {
-    id: '123',
-    username: 'krisf451',
-    userImg: faker.image.avatar(),
-    img: faker.image.avatar(),
-    caption: 'YOOOO',
-  },
-  {
-    id: '123',
-    username: 'krisf451',
-    userImg: faker.image.avatar(),
-    img: faker.image.avatar(),
-    caption: 'YOOOO',
-  },
-]
+import { useState, useEffect } from 'react'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { db } from '../firebase'
 
 function Posts() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          setPosts(snapshot.docs)
+        }
+      ),
+    [db]
+  )
+
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
